146144723e8fbeb7341ddfde339bab3cca9d4eb2
daniel <lesamessipa@gmail.com>
2022-10-16 00:51:28 +0200

    first commit install of breeze and make model and factory for database

---
+++ database/migrations/2022_10_15_221416_create_order_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+
+            $table->foreignIdFor(\App\Models\Order::class);
+            $table->foreignIdFor(\App\Models\Product::class);
+
+            $table->integer('total_quantity');
+            $table->integer('total_price');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
